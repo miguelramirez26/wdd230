4aaf9cfb827d322a1712f0a31e0b4de477b9597d
miguelramirez26 <26ramirez.m@gmail.com>
2023-10-04 15:19:11 -0600

    home page enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -7,3 +7,11 @@ document.querySelector('#currentYear').textContent = currentYear;
 let text = document.lastModified;
 const date = new Date(document.lastModified);
 document.querySelector('#lastModified').textContent = text;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
