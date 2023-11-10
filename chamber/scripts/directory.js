3388be043e0caccd282fa8b11dbb391ec6d3cf1c
miguelramirez26 <26ramirez.m@gmail.com>
2023-11-10 14:14:22 -0700

    creating directory page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,39 @@
+const url = 'https://miguelramirez26.github.io/wdd230/chamber/data/members.json';
+const grid = document.querySelector('.grid');
+const displayCompanies = (companies) => {
+    companies.forEach((company) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let portrait = document.createElement('img');
+        let websiteurl = document.createElement('a');
+
+        name.textContent = `${company.name}`;
+        address.textContent = `${company.address}`;
+        phone.textContent = `${company.phone}`;
+        websiteurl.href = company.websiteurl;
+        websiteurl.textContent = company.websiteurl;
+        portrait.setAttribute('src', company.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${company.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(websiteurl);
+        card.appendChild(portrait);
+
+        grid.appendChild(card); 
+    });
+}
+
+async function getCompanyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCompanies(data.companies);
+}
+
+getCompanyData();
